88d65df882966863ab3054ae5a6fe7f653e8dfa4
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-03-14 15:38:04 +0530

    Largest Gap

---
+++ largest_gap.cpp
@@ -0,0 +1,8 @@
+int solve(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+    int ans=0;
+    for(int i=0;i<nums.size()-1;i++){
+        ans=max(ans,nums[i+1]-nums[i]);
+    }
+    return ans;
+}
