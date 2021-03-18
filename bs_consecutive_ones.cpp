cddf4f5758a5603b807ae0df9344843c8d2c490d
aditya_tiwary <63966104+AdityaShekharTiwary@users.noreply.github.com>
2021-03-18 23:51:48 +0530

    Consecutive ones

---
+++ bs_consecutive_ones.cpp
@@ -0,0 +1,22 @@
+bool solve(vector<int>& nums) {
+    int freq = 0, cnt = 0, mx = 0;
+    for (auto x : nums) {
+        if (x == 1) {
+            freq++;
+        }
+    }
+    for (auto x : nums) {
+        if (x == 1) {
+            cnt++;
+        } else {
+            mx = max(mx, cnt);
+            cnt = 0;
+        }
+        mx = max(mx, cnt);
+    }
+    if (freq == mx) {
+        return true;
+    }
+    return false;
+
+}
